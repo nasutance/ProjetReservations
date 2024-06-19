8c8fda65522c04405c82a2305b70c4022ee3e973
nasutance <n.nasuti@gmail.com>
2024-06-19 18:06:18 +0200

    Modèle Show et migration et donnees

---
+++ catalogue/models/show.py
@@ -0,0 +1,18 @@
+# catalogue/models/show.py
+from django.db import models
+from .location import Location
+
+class Show(models.Model):
+    show_id = models.BigAutoField(primary_key=True)
+    slug = models.CharField(max_length=60, unique=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=255, null=True)
+    poster_url = models.CharField(max_length=255, null=True)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, related_name='shows')
+    duration = models.SmallIntegerField(null=True)
+    created_in = models.CharField(max_length=4, null=True)
+    bookable = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+
+    class Meta:
+        db_table = "shows"
